d1a1d3ad5395b057c2b5a51d8298b62201534883
243437 <243437@KB37-1-303-C11>
2026-09-09 11:43:40 +0300

    финальная версия

--- ISIP224_Akhlamov/Program.cs
+++ ISIP224_Akhlamov/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace ISIP224_Akhlamov
     {
         static void Main(string[] args)
         {
+
+            Dictionary<string,int> wastes = new Dictionary<string,int>();
+            List<int> Total_money = new List<int>();
+            int count_of_operatons = 0;
+            while (count_of_operatons < 2 || count_of_operatons > 40)
+            {
+                count_of_operatons = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < count_of_operatons; i++)
+            {
+                
+                Console.WriteLine("Напишите товар и потраченные деньги Пример: Влажные салфетки Лента; 235");
+                string user_input = Console.ReadLine();
+                wastes.Add(user_input.Split(';')[0], Convert.ToInt32(user_input.Split(';')[1]));
+                Total_money.Add(Convert.ToInt32(user_input.Split(';')[1]));
+                
+            }
+
+
         }
     }
 }
